78e55ee4d70247169eb8abfbea2e0da8b6e1d902
Zim <obizimuzo@gmail.com>
2022-06-08 15:26:52 -0700

    custom font & _document

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,18 +1,22 @@
+import styles from "./nav.module.css";
+
 export const Navbar = () => {
   return (
-    <header className="header">
-      <h1>V/A</h1>
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <h1>V/A</h1>
 
-      <ul className="nav_items">
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
+        <ul className={styles.nav_list}>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
 
-      <ul className="socials">
-        <li>Tw</li>
-        <li>Fb</li>
-        <li>Li</li>
-      </ul>
+        <ul className={styles.nav_socials}>
+          <li>Tw</li>
+          <li>Fb</li>
+          <li>Li</li>
+        </ul>
+      </nav>
     </header>
   );
 };
