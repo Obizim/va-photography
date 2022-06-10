53cd617d81dcd0a8573f71501ce4b84ca70b0aae
Zim <obizimuzo@gmail.com>
2022-06-09 17:45:21 -0700

    arrows

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,6 +19,15 @@ const Home: NextPage = () => {
           <p className={styles.desc}>
             Event/Portrait photographer & film maker
           </p>
+
+          <div className={styles.arrows}>
+            <div className={styles.arrow_left}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M7.828 11H20v2H7.828l5.364 5.364-1.414 1.414L4 12l7.778-7.778 1.414 1.414z" fill="#fff"/></svg>
+            </div>
+            <div className={styles.arrow_right}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" fill="#fff"/></svg>
+            </div>
+          </div>
         </div>
 
         <div className={styles.works}>
