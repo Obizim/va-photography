0800346d8dfa70030531a788940d7b81f9a6e7c8
Zim <obizimuzo@gmail.com>
2022-06-14 10:27:48 -0700

    eslint issues fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Tiro+Tamil&display=swap" rel="stylesheet"></link>
+        <title>Vitor Abiola - Portfolio</title>
       </Head>
       <body>
         <Main />
